403ca83d1362c3c37ac9e070a2d20db13051ad7d
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-01-17 08:44:55 +0100

    Add gulp installation

--- install/08-node.sh
+++ install/08-node.sh
@@ -5,3 +5,7 @@ if test ! -f /usr/local/bin/node; then
     brew install node@8
     brew link node@8 --force
 fi
+if test ! -f /usr/local/bin/gulp; then
+    echo Install gulp
+    npm install gulp -g
+fi
