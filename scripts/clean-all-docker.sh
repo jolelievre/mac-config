557994e213c7db516a71ddb950af263166b79eee
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-12-11 18:02:42 +0100

    Update docker clean script

--- scripts/clean-all-docker.sh
+++ scripts/clean-all-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Stop all containers
-docker kill $(docker ps -q)
+docker kill $(docker ps -q -a)
 
 # Delete all stopped containers
 docker rm $(docker ps -a -q)
@@ -10,4 +10,13 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q -f dangling=true)
 
 # Delete ALL images
-docker rmi $(docker images -q)
+docker rmi $(docker images -a -q)
+
+# Delete volumes
+docker volume rm $(docker volume ls -qf dangling=true)
+docker volume rm $(docker volume ls -q)
+
+# Little pruning
+docker system prune --volumes -a -f
+docker volume prune -f
+docker system prune -a -f
