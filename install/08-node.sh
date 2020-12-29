eababf53d666816cc99143c5ff2981ebf682bada
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-12-29 17:23:18 +0100

    No gulp install any more

--- install/08-node.sh
+++ install/08-node.sh
@@ -5,7 +5,3 @@ if test ! -f /usr/local/bin/node; then
     brew install node@10
     brew link node@10 --force
 fi
-if test ! -f /usr/local/bin/gulp; then
-    echo Install gulp
-    npm install gulp -g
-fi
