2a683b81b8eea53ba832d9f1e70b82c871490d48
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-05-10 14:43:25 +0200

    Update Symfony CLI install

--- install/10-symfony.sh
+++ install/10-symfony.sh
@@ -2,6 +2,5 @@
 
 if test ! -f /usr/local/bin/symfony; then
     echo "Installing Symfony installer command"
-    curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
-    chmod a+x /usr/local/bin/symfony
+    wget https://get.symfony.com/cli/installer -O - | bash
 fi
