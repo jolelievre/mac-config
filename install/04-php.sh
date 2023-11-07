e7e2bf4abab4946df40bc944cdaf519711a5b27c
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-11-07 20:15:00 +0100

    Add PHP 8.3 in PHP versions

--- install/04-php.sh
+++ install/04-php.sh
@@ -31,7 +31,7 @@ done
 if test $# -gt 0; then
     phpVersions=$1
 else
-    phpVersions="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2"
+    phpVersions="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3"
 fi
 
 lastInstalledVersion=''
