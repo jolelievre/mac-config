72b8f3e6e19d3539f3905ccaf4e689c74ce3ae61
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-11-16 13:34:57 +0100

    Update blackfire install scripts

--- install/09-blackfire.sh
+++ install/09-blackfire.sh
@@ -2,51 +2,64 @@
 
 BASEDIR=$(dirname "$0")
 
-#brew tap blackfireio/homebrew-blackfire
-if test ! -d /usr/local/Cellar/blackfire-agent; then
-    brew install blackfire-agent
+brew tap blackfireio/homebrew-blackfire
+if test ! -d /usr/local/Cellar/blackfire; then
+    brew install blackfire
 fi
 
-# This is the equivalent to sudo blackfire-agent -register
-cp $BASEDIR/../blackfire/agent /usr/local/etc/blackfire/agent
+# Register agent config
+if [ -f /usr/local/etc/blackfire/agent ]; then
+  echo Blackfire agent config already present, to reconfigure it please run: blackfire agent:config
+else
+  blackfire agent:config
+fi
 
-# Copy services config and load for the first time
-ln -sfv /usr/local/opt/blackfire-agent/*.plist ~/Library/LaunchAgents/
-launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.blackfire-agent.plist
+# Start the service
+echo Starting blackfire service
+brew services start blackfire
 
-# Init client
-cp $BASEDIR/../blackfire/.blackfire.ini ~
+# Init client config
+if [ -f ~/.blackfire.ini ]; then
+  echo Blackfire client config already present, to reconfigure it please run: blackfire client:config
+else
+  blackfire client:config
+fi
 
-phpVersions="56 71 72 73 74 80"
+# phpVersions="56 71 72 73 74 80 81 82"
+phpVersions="74 80 81 82"
 for phpVersion in $phpVersions; do
     blackfireDir="/usr/local/Cellar/blackfire-php$phpVersion"
     if test ! -d $blackfireDir; then
         brew install blackfire-php$phpVersion
     fi
-    
-    blackfireVersions=`ls $blackfireDir`
-    libLink=$blackfireDir/blackfire.so
-    for blackfireVersion in $blackfireVersions; do
-        if test -d $blackfireDir/$blackfireVersion; then
-            rm -f $libLink
-            ln -s $blackfireDir/$blackfireVersion/blackfire.so $libLink
-        fi
-    done
 
-    installedPhpVersions=`brew list | grep php | sed s_\ __g | sed s_php@__g`
-    for versionNumber in $installedPhpVersions; do
-        minimalVersion=`echo $versionNumber | sed "s/\.//g" | cut -c 1-2`
-        if test $minimalVersion -eq $phpVersion; then
-            iniPath="/usr/local/etc/php/$versionNumber/php.ini"
-            if test -f $iniPath; then
-                cat $iniPath | grep blackfire.so > /dev/null
-                hasBlackfire=$?
-                if test $hasBlackfire -ne 0; then
-                    echo "Install Blackfire lib $libLink in $iniPath"
-                    echo "" >> $iniPath
-                    echo "extension=$libLink" >> $iniPath
-                fi
-            fi
-        fi
-    done
+# Previous manual install of the lib, commented for now
+#    blackfireVersions=`ls $blackfireDir`
+#    libLink=$blackfireDir/blackfire.so
+#    for blackfireVersion in $blackfireVersions; do
+#        if test -d $blackfireDir/$blackfireVersion; then
+#            rm -f $libLink
+#            ln -s $blackfireDir/$blackfireVersion/blackfire.so $libLink
+#        fi
+#    done
+
+#    installedPhpVersions=`brew list | grep php | sed s_\ __g | sed s_php@__g`
+#    for versionNumber in $installedPhpVersions; do
+#        minimalVersion=`echo $versionNumber | sed "s/\.//g" | cut -c 1-2`
+#        if test $minimalVersion -eq $phpVersion; then
+#            iniPath="/usr/local/etc/php/$versionNumber/php.ini"
+#            if test -f $iniPath; then
+#                cat $iniPath | grep blackfire.so > /dev/null
+#                hasBlackfire=$?
+#                if test $hasBlackfire -ne 0; then
+#                    echo "Install Blackfire lib $libLink in $iniPath"
+#                    echo "" >> $iniPath
+#                    echo "extension=$libLink" >> $iniPath
+#                fi
+#            fi
+#        fi
+#    done
 done
+
+echo Restart apache
+sudo apachectl restart
